501f7d1fa57626fa14192abc8d6fe6fbf0a36baf
wrvargas20students.desu.edu <wrvargas20students.desu.edu>
2023-05-05 21:58:47 -0400

    I made changes

--- src/main/java/com/github/curriculeon/BinarySearchTree.java
+++ src/main/java/com/github/curriculeon/BinarySearchTree.java
@@ -13,31 +13,105 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     private Value get(BinarySearchTreeNode<Key, Value> x, Key key) {
-        return null; // TODO
+        BinarySearchTreeNode<Key, Value> compare = root;
+        if(root == null) return null;
+        while (compare != null){
+            if (compare.getKey().equals(key))
+                return compare.getValue();
+            if (compare.getKey().compareTo(key) > 0)
+                compare = compare.getLeft();
+            else if (compare.getKey().compareTo(key) < 0)
+                compare = compare.getRight();
+        }
+        return null;
     }
 
     private BinarySearchTreeNode<Key, Value> put(BinarySearchTreeNode<Key, Value> x, Key key, Value value) {
-        return null; // TODO
+        if (key == null || value == null)
+            throw new NullPointerException();
+        if (root == null)
+            x = new BinarySearchTreeNode<>(key, value);
+        else
+            while (root != null)
+                if (root.getKey().compareTo(key) < 0){
+                    if (root.getRight() == null){
+                        root.setRight(new BinarySearchTreeNode<>(key, value));
+                        break;
+                    }
+                    root = root.getRight();
+                }
+                else if (root.getKey().compareTo(key) > 0){
+                    if (root.getLeft() == null){
+                        root.setLeft(new BinarySearchTreeNode<>(key, value));
+                        break;
+                    }
+                    root = root.getLeft();
+                }
+                else{
+                    root.setValue(value);
+                    break;
+                }
+        size++;
+        return x;
     }
 
     private BinarySearchTreeNode<Key, Value> min(BinarySearchTreeNode<Key, Value> x) {
-        return null; // TODO
+        while(root.getLeft() != null) root = root.getLeft();
+        return root;
     }
 
     private BinarySearchTreeNode<Key, Value> max(BinarySearchTreeNode<Key, Value> x) {
-        return null; // TODO
+        while(root.getRight() != null) root = root.getRight();
+        return root;
     }
 
     private BinarySearchTreeNode<Key, Value> deleteMin(BinarySearchTreeNode<Key, Value> x) {
-        return null; // TODO
+        root = min(root);
+        if (root.getRight() == null) {
+
+            if (root.hasprevious()) {
+                root = root.getPrevious();
+                root.setLeft(null);
+            } else root = null;
+        }
+        else root = root.getRight();
+
+        return root;
     }
 
     private BinarySearchTreeNode<Key, Value> deleteMax(BinarySearchTreeNode<Key, Value> x) {
-        return null; // TODO
+        root = max(root);
+        if (root.getLeft() == null) {
+            if (root.hasprevious()) {
+                root = root.getPrevious();
+                root.setRight(null);
+
+
+            } else root = null;
+        }
+        else root = root.getLeft();
+        return root;
     }
 
     private BinarySearchTreeNode<Key, Value> delete(BinarySearchTreeNode<Key, Value> x, Key key) {
-        return null; // TODO
+        while (root != null){
+            if (root.getKey().equals(key)){
+                if (root.getRight() != null){
+                    root.getRight().setLeft(root.getLeft());
+                    root = root.getRight();
+                }
+                else if (root.getLeft() != null)
+                    root = root.getLeft();
+                size--;
+                return root;
+            }
+            if (root.getKey().compareTo(key) > 0)
+                root = root.getLeft();
+            else if (root.getKey().compareTo(key) < 0)
+                root = root.getRight();
+        }
+        size--;
+        return null;
     }
 
     public int size() {
